0d8470696a56ea0ef0ec03e660c6703867c4688e
Bess Campbell <bess.k.campbell@gmail.com>
2020-12-11 10:29:52 -0800

    Show functioning UI

--- ProjectName/Program.cs
+++ ProjectName/Program.cs
@@ -7,12 +7,16 @@ namespace Bakery
     {
         static void Main()
         {
-            Console.WriteLine("Welcome to Pierre's Bakery! Today we have my famous country loaf for $5 or if you buy 2 you get one free. We also have the always delightful berry danish for $2 or 3 for $5.");
+            Console.WriteLine("Welcome to Pierre's Bakery! Today we have our famous country loaf for $5 or if you buy 2 you get 1 free. We also have the always delightful berry danish for $2 or 3 for $5.");
             Console.WriteLine("How many loaves of bread would you like?");
             string strLoaves = Console.ReadLine();
-            Bread breadOrder = new Bread(int.Parse(strLoaves)); 
-            
-
+            Bread breadOrder = new Bread(int.Parse(strLoaves));
+            Console.WriteLine("And how many pastries can I get you today?");
+            string strPastries = Console.ReadLine();
+            Pastry pastryOrder = new Pastry(int.Parse(strPastries));
+            breadOrder.TotalBread();
+            pastryOrder.TotalPastries();
+            Console.WriteLine($"So that's {breadOrder.Loaves} loaves for ${breadOrder.BreadTotal} and {pastryOrder.PastryTotal} pastries for ${pastryOrder.PastryTotal} your total comes to ${breadOrder.BreadTotal + pastryOrder.PastryTotal}. Thank you so much for visiting Pierre's!");
         }
     }
 }
