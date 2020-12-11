3dcbbadc7f1d601f03f5ac7edac44b2392cefb0a
Bess Campbell <bess.k.campbell@gmail.com>
2020-12-11 10:29:52 -0800

    Show pastry constructor is passing'

--- ProjectName/Models/Pastry.cs
+++ ProjectName/Models/Pastry.cs
@@ -1,7 +1,15 @@
+
+
 namespace Bakery.Models
 {
     public class Pastry
     {
-        
+        public int Pastries { get; }
+        public int PastryTotal { get; set; }
+        public Pastry(int pastries)
+        {
+            Pastries = pastries;
+            PastryTotal = 0;
+        }   
     }
 }
