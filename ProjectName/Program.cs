b3f778f45f2f8b3ba6faf26772bdca8841042cef
Bess Campbell <bess.k.campbell@gmail.com>
2020-12-11 10:29:52 -0800

    Add branching to highlight deals

--- ProjectName/Program.cs
+++ ProjectName/Program.cs
@@ -10,13 +10,33 @@ namespace Bakery
             Console.WriteLine("Welcome to Pierre's Bakery! Today we have our famous country loaf for $5 or if you buy 2 you get 1 free. We also have the always delightful berry danish for $2 or 3 for $5.");
             Console.WriteLine("How many loaves of bread would you like?");
             string strLoaves = Console.ReadLine();
-            Bread breadOrder = new Bread(int.Parse(strLoaves));
+            int numLoaves = int.Parse(strLoaves);
+            if (numLoaves % 3 == 2)
+            {
+                Console.WriteLine("Are you sure you wouldn't like to get one more loaf? It would be free with our buy 2 get 1 free deal. If you would like to add another loaf press ['Y' for yes] or press ['Enter' for no].");
+                string moreBread = Console.ReadLine();
+                if (moreBread == "Y" || moreBread == "y")
+                {
+                    numLoaves += 1;
+                }
+            }
+            Bread breadOrder = new Bread(numLoaves);
             Console.WriteLine("And how many pastries can I get you today?");
             string strPastries = Console.ReadLine();
-            Pastry pastryOrder = new Pastry(int.Parse(strPastries));
+            int numPastries = int.Parse(strPastries);
+             if (numPastries % 3 == 2)
+            {
+                Console.WriteLine("Are you sure you wouldn't like to get one more pastry? It would cost only $1 more! If you would like to add another pastry press ['Y' for yes] or press ['Enter' for no].");
+                string anotherPastry = Console.ReadLine();
+                if (anotherPastry == "Y" || anotherPastry == "y")
+                {
+                    numPastries += 1;
+                }
+            }
+            Pastry pastryOrder = new Pastry(numPastries);
             breadOrder.TotalBread();
             pastryOrder.TotalPastries();
-            Console.WriteLine($"So that's {breadOrder.Loaves} loaves for ${breadOrder.BreadTotal} and {pastryOrder.PastryTotal} pastries for ${pastryOrder.PastryTotal} your total comes to ${breadOrder.BreadTotal + pastryOrder.PastryTotal}. Thank you so much for visiting Pierre's!");
+            Console.WriteLine($"So that's {breadOrder.Loaves} loaves for ${breadOrder.BreadTotal} and {pastryOrder.Pastries} pastries for ${pastryOrder.PastryTotal} your total comes to ${breadOrder.BreadTotal + pastryOrder.PastryTotal}. Thank you so much for visiting Pierre's!");
         }
     }
 }
